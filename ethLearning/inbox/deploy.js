318229702924bef56091a0a9a2e73284b6862d9a
hitesharora2 <41942496+hitesharora2@users.noreply.github.com>
2020-05-02 17:03:21 +0200

    deployment worked

--- ethLearning/inbox/deploy.js
+++ ethLearning/inbox/deploy.js
@@ -1,10 +1,10 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'call glow acoustic vintage front ring trade assist shuffle mimic volume reject',
-  'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+  'sustain body pet island music narrow grape apology sponsor winner actual amazing',
+  'https://rinkeby.infura.io/v3/9075883ee7cf4218971e0aca63928d9d'
 );
 const web3 = new Web3(provider);
 
@@ -14,8 +14,8 @@ const deploy = async () => {
   console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
-    .send({ gas: '1000000', from: accounts[0] });
+    .deploy({ data: '0x' +bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
 };
